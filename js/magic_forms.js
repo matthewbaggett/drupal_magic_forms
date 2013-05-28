72aeae9f395a43ccf1141dfb457753241cec3b80
matt <matt@adefee90-54ce-384d-8f2b-910887fe9068>
2013-05-28 15:41:20 +0000

    Automatic reformat
    git-svn-id: http://btehlwman01.btengageit.net:9880/Portal/trunk/Portal%20Drupal/sites/all/modules/magic_forms@181 adefee90-54ce-384d-8f2b-910887fe9068

--- js/magic_forms.js
+++ js/magic_forms.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
   var form = jQuery('form.magic_form');
-  jQuery('select, input, textarea', form).bind('change',function(){
+  jQuery('select, input, textarea', form).bind('change', function () {
     jQuery(this).addClass('changed');
   });
 });
